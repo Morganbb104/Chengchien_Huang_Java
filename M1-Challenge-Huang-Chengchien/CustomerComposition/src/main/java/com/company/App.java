df6ffeda485e6be3ae14249337eca1921ac5acb4
scouring pad <hcc780921@gmail.com>
2022-07-03 23:09:10 -0700

    CustomerComposition App completed

--- M1-Challenge-Huang-Chengchien/CustomerComposition/src/main/java/com/company/App.java
+++ M1-Challenge-Huang-Chengchien/CustomerComposition/src/main/java/com/company/App.java
@@ -1,4 +1,25 @@
 package com.company;
+import com.company.Address;
+import com.company.Customer;
 
 public class App {
+    public static void main(String[] args) {
+
+        Address billingAddress= new Address("1675","SE street","Shoreline","WA","98004");
+        Address shippingAddress = new Address("145","NW Ave","Tacoma","WA","14785");
+
+//customer information
+        Customer cch = new Customer(
+          "Chengchien",
+          "Huang",
+          "hcc@gmail.com",
+          "0952090909",
+                billingAddress,
+                shippingAddress,
+                true
+
+        );
+
+        System.out.println(cch);
+    }
 }
