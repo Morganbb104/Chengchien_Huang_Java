77349a686b6699a5ab6377bc9b9970f05ae3b8a0
scouring pad <hcc780921@gmail.com>
2022-07-05 21:02:20 -0700

    Constable Test all completed

--- M1-Challenge-Huang-Chengchien/RPGInheritance/src/test/java/com/company/ConstableTest.java
+++ M1-Challenge-Huang-Chengchien/RPGInheritance/src/test/java/com/company/ConstableTest.java
@@ -10,49 +10,38 @@ public class ConstableTest {
     Constable constable;
 
     @Before
-    public void setUp(){
-        constable = new Constable("Thomas");
+    public void setUp() {
+        constable = new Constable("Stone");
     }
-
     @Test
-    public void attack(){
-        Constable Jacob = new Constable();
-        Constable.attack(constable("Jacob"));
-        int actualOutput = Jacob.getHealth();
-        int expectedOutput = 100;
+    public void whileGotAttackedShouldLose5blood() {
+        Constable Cheng = new Constable("Cheng");
+        Cheng.attack(constable);
+        int actualOutput = constable.getHealth();
+        int expectedOutput = 95;
         assertEquals(expectedOutput,actualOutput);
+
     }
 
-//
-//    @Test
-//    public void arrest(){
-//        Constable constable = new Constable constable("Jacob");
-//
-//        String expectedOutput = Thomas+"arrested"+"Joe"+".";
-//        String actualOutput = "Thomas arrested Joe.";
-//        assertEquals(expectedOutput,actualOutput);
-//
-//    }
+
+
     @Test
-    public void arrest() {
-        Constable Cheng = new Constable();
+    public void whileArrestedShouldReturnFalse() {
+        Constable Cheng = new Constable("Cheng");
         Cheng.arrest(constable);
         boolean expectedValue = false;
         boolean actualValue = constable.isArrested();
         assertEquals(expectedValue, actualValue);
-}
-
-    private void constable(String jacob) {
     }
 
     @Test
-public void ReturnTheFalseInisJurisdiction() {
-    Constable constable = new Constable();
-    constable.isJurisdiction();
-    boolean expectedOutput = false;
-    boolean actualOutput = constable.isJurisdiction();
-    assertEquals(expectedOutput, actualOutput);
-}
+    public void ReturnTheFalseInisJurisdiction() {
+        Constable constable = new Constable();
+        constable.isJurisdiction();
+        boolean expectedOutput = false;
+        boolean actualOutput = constable.isJurisdiction();
+        assertEquals(expectedOutput, actualOutput);
+    }
 
     @Test
     public void ReturnTheNewValueInsetJurisdiction() {
